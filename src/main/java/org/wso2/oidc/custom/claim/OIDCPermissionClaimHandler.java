9a90527fb8709969a73ee52a41005f6667bcda6c
Mifraz Murthaja <mmtech95@gmail.com>
2020-09-11 01:02:39 +0530

    restructure permissions

--- src/main/java/org/wso2/oidc/custom/claim/OIDCPermissionClaimHandler.java
+++ src/main/java/org/wso2/oidc/custom/claim/OIDCPermissionClaimHandler.java
@@ -2,6 +2,7 @@ package org.wso2.oidc.custom.claim;
 
 import com.nimbusds.jwt.JWTClaimsSet;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
@@ -29,8 +30,10 @@ import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.apache.commons.collections.MapUtils.isNotEmpty;
 
@@ -39,10 +42,19 @@ import static org.apache.commons.collections.MapUtils.isNotEmpty;
  */
 public class OIDCPermissionClaimHandler extends DefaultOIDCClaimsCallbackHandler {
 
-    private static final String PERMISSION_CLAIM = "http://wso2.org/claims/permission";
     private static final Log log = LogFactory.getLog(OIDCPermissionClaimHandler.class);
     private static final String OAUTH2 = "oauth2";
+    private static final String LOCAL_DIALECT = "http://wso2.org/claims";
     private static final String OIDC_DIALECT = "http://wso2.org/oidc/claim";
+    private static final String PERMISSION_CLAIM = "http://wso2.org/claims/permission";
+    private static final String PERMISSION_PATH = "/permission";
+    private static final String SUPER_ADMIN_PERMISSION_PATH = "/permission/protected";
+    private static final String ADMIN_PERMISSION_PATH = "/permission/admin";
+    private static final String APPLICATION_PERMISSION_PATH = "/permission/applications";
+    private static final String SUPER_ADMIN_PERMISSION = "protected";
+    private static final String ADMIN_PERMISSION = "admin";
+    private static final String APPLICATION_PERMISSION = "applications";
+    private static final String OTHER_PERMISSION = "other";
 
     @Override
     public JWTClaimsSet handleCustomClaims(JWTClaimsSet.Builder jwtClaimsSetBuilder,
@@ -119,28 +131,8 @@ public class OIDCPermissionClaimHandler extends DefaultOIDCClaimsCallbackHandler
                 }
             }
 
-            AuthorizationManager authorizationManager = realm.getAuthorizationManager();
-            Map<String, List<String>> permissionList = new HashMap<>();
-            for (String claimUri : userClaimsInOidcDialect) {
-                if (claimUri.contains(PERMISSION_CLAIM)) {
-                    String permissionRootPath = claimUri.replace("http://wso2.org/claims", "");
-                    if (log.isDebugEnabled()) {
-                        log.debug("Retrieving permissions for " + permissionRootPath);
-                    }
-                    String[] permissions = authorizationManager
-                            .getAllowedUIResourcesForUser(MultitenantUtils.getTenantAwareUsername(userName),
-                                    permissionRootPath);
-                    if (log.isDebugEnabled()) {
-                        if (ArrayUtils.isEmpty(permissions)) {
-                            log.debug("Permission list is empty for " + permissionRootPath);
-                        } else {
-                            log.debug("Retrieved permission list for " + permissionRootPath + ": " +
-                                    Arrays.asList(permissions));
-                        }
-                    }
-                    permissionList.put(permissionRootPath, Arrays.asList(permissions));
-                }
-            }
+            Map<String, List<String>> permissionList =
+                    createPermissionStructure(userClaimsInOidcDialect, userName, realm);
 
             if (!permissionList.isEmpty()) {
                 jwtClaimsSetBuilder.claim("permissions", permissionList);
@@ -149,8 +141,6 @@ public class OIDCPermissionClaimHandler extends DefaultOIDCClaimsCallbackHandler
             log.error(
                     "Error while obtaining service provider for tenant domain: " + spTenantDomain + " client id: "
                             + clientId, e);
-        } catch (UserStoreException e) {
-            log.error("Error while retrieving user claim in local dialect for user: " + userName, e);
         } catch (IdentityException e) {
             log.error("Error while obtaining user realm for for user: " + userName + " in tenant domain: " +
                     userTenantDomain, e);
@@ -191,7 +181,6 @@ public class OIDCPermissionClaimHandler extends DefaultOIDCClaimsCallbackHandler
     private List<String> getUserClaimsInOidcDialect(String spTenantDomain, String clientId, String[] requestedScopes)
             throws IdentityApplicationManagementException, ClaimMetadataException {
 
-        List<String> claimURIList = new ArrayList<>();
         Map<String, Object> userClaimsInOidcDialect = new HashMap<>();
         ServiceProvider serviceProvider = getServiceProvider(spTenantDomain, clientId);
         ClaimMapping[] requestClaimMappings = getRequestedClaimMappings(serviceProvider);
@@ -226,5 +215,76 @@ public class OIDCPermissionClaimHandler extends DefaultOIDCClaimsCallbackHandler
             }
         }
         return claimURIList;
+    }
+
+    private Map<String, List<String>> createPermissionStructure(List<String> userClaimsInOidcDialect, String userName,
+                                                                UserRealm realm) {
+
+        Map<String, List<String>> permissionList = new HashMap<>();
+        Set<String> superAdminPermissionList = new HashSet<>();
+        Set<String> adminPermissionList = new HashSet<>();
+        Set<String> applicationPermissionList = new HashSet<>();
+        Set<String> otherPermissionList = new HashSet<>();
+
+        try {
+            AuthorizationManager authorizationManager = realm.getAuthorizationManager();
+
+            for (String claimUri : userClaimsInOidcDialect) {
+                if (claimUri.contains(PERMISSION_CLAIM)) {
+                    String permissionRootPath = claimUri.replace(LOCAL_DIALECT, "");
+                    if (log.isDebugEnabled()) {
+                        log.debug("Retrieving permissions for " + permissionRootPath);
+                    }
+                    String[] permissions = authorizationManager
+                            .getAllowedUIResourcesForUser(MultitenantUtils.getTenantAwareUsername(userName),
+                                    permissionRootPath);
+                    if (log.isDebugEnabled()) {
+                        if (ArrayUtils.isEmpty(permissions)) {
+                            log.debug("Permission list is empty for " + permissionRootPath);
+                        } else {
+                            log.debug("Retrieved permission list for " + permissionRootPath + ": " +
+                                    Arrays.asList(permissions));
+                        }
+                    }
+
+                    for (String permission : permissions) {
+                        if (permission.contains(SUPER_ADMIN_PERMISSION_PATH)) {
+                            superAdminPermissionList.add(getPermissionLeaf(SUPER_ADMIN_PERMISSION_PATH, permission));
+                        } else if (permission.contains(ADMIN_PERMISSION_PATH)) {
+                            adminPermissionList.add(getPermissionLeaf(ADMIN_PERMISSION_PATH, permission));
+                        } else if (permission.contains(APPLICATION_PERMISSION_PATH)) {
+                            applicationPermissionList.add(getPermissionLeaf(APPLICATION_PERMISSION_PATH, permission));
+                        } else {
+                            otherPermissionList.add(getPermissionLeaf(PERMISSION_PATH, permission));
+                        }
+                    }
+                }
+            }
+            if (!superAdminPermissionList.isEmpty()) {
+                permissionList.put(SUPER_ADMIN_PERMISSION, new ArrayList<>(superAdminPermissionList));
+            }
+            if (!adminPermissionList.isEmpty()) {
+                permissionList.put(ADMIN_PERMISSION, new ArrayList<>(adminPermissionList));
+            }
+            if (!applicationPermissionList.isEmpty()) {
+                permissionList.put(APPLICATION_PERMISSION, new ArrayList<>(applicationPermissionList));
+            }
+            if (!otherPermissionList.isEmpty()) {
+                permissionList.put(OTHER_PERMISSION, new ArrayList<>(otherPermissionList));
+            }
+
+        } catch (UserStoreException e) {
+            log.error("Error while retrieving user claim in local dialect for user: " + userName, e);
+        }
+        return permissionList;
+    }
+
+    private String getPermissionLeaf(String path, String permission) {
+
+        permission = permission.replace(path, "");
+        if (StringUtils.isEmpty(permission)) {
+            permission = "/";
+        }
+        return permission;
     }
 }
